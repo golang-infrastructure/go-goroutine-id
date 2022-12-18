761a923afeab844efab979dc902c2c5f2db57afd
CC11001100 <CC11001100@qq.com>
2022-12-19 03:35:51 +0800

    init

---
+++ goroutine_id.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+// GetGoroutineID 获取当前协程的ID
+func GetGoroutineID() (int, error) {
+	var buf [64]byte
+	n := runtime.Stack(buf[:], false)
+	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
+	id, err := strconv.Atoi(idField)
+	if err != nil {
+		return 0, fmt.Errorf("cannot get goroutine id: %v", err)
+	}
+	return id, nil
+}
---
+++ goroutine_id_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+)
+
+// 只要能成功获取到id就认为是没问题
+func TestGetGoroutineID(t *testing.T) {
+	var goroutineId int
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		goroutineId, _ = GetGoroutineID()
+	}()
+	wg.Wait()
+
+	assert.True(t, goroutineId != 0)
+}
