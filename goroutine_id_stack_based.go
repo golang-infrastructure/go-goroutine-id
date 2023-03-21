f5f7627f03ffd75601786343277385dc506af60c
CC11001100 <CC11001100@qq.com>
2023-03-21 15:33:27 +0800

    Update goroutine_id_stack_based.go

--- goroutine_id_stack_based.go
+++ goroutine_id_stack_based.go
@@ -7,17 +7,17 @@ import (
 	"strings"
 )
 
-// GetGoroutineId 获取当前协程的ID
-func GetGoroutineId() (int, error) {
-	id, err := strconv.Atoi(GetGoroutineIdAsString())
+// GetGoroutineID 获取当前协程的ID
+func GetGoroutineID() (int, error) {
+	id, err := strconv.Atoi(GetGoroutineIDAsString())
 	if err != nil {
 		return 0, fmt.Errorf("cannot get goroutine id: %v", err)
 	}
 	return id, nil
 }
 
-// GetGoroutineIdAsString 获取当前协程的ID，以字符串的形式返回
-func GetGoroutineIdAsString() string {
+// GetGoroutineIDAsString 获取当前协程的ID，以字符串的形式返回
+func GetGoroutineIDAsString() string {
 	var buf [64]byte
 	n := runtime.Stack(buf[:], false)
 	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
